2fe62ee4a21a967363fed7058ae597d60af6b7ff
Darren Deepak <dr266927@dal.ca>
2026-08-17 01:10:52 +0530

    Prepare server for deployment

--- server/server.js
+++ server/server.js
@@ -2,15 +2,13 @@ import "dotenv/config";
 import app from "./src/app.js";
 import connectDatabase from "./src/config/db.js";
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 async function startServer() {
   await connectDatabase();
 
-  app.listen(port, () => {
-    console.log(
-      `Server running on port ${port}`
-    );
+  app.listen(port, "0.0.0.0", () => {
+    console.log(`Server running on port ${port}`);
   });
 }
 
